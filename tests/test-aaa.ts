d3b2b0fb71b0237b2dc7660e4aeec8c59d8c8945
jj <jj@ondemandenv.dev>
2024-10-14 22:25:33 -0400

    .13

---
+++ tests/test-aaa.ts
@@ -0,0 +1,13 @@
+// Successfully instantiate OndemandContractsSandbox without errors
+import {OndemandContractsSandbox} from "../lib/OndemandContractsSandbox";
+import {App} from "aws-cdk-lib";
+
+
+
+
+// Attempt to instantiate OndemandContractsSandbox when an instance already exists
+it('should throw an error when trying to instantiate OndemandContractsSandbox if an instance already exists', () => {
+    const app = new App();
+    new OndemandContractsSandbox(app);
+    expect(() => new OndemandContractsSandbox(app)).toThrow(`There is already a Construct with name 'OndemandContractsSandbox' in App`);
+});
