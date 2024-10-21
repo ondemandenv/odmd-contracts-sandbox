06ded48fb090afdb91badf36031a046588e1ce15
jj <jj@ondemandenv.dev>
2024-10-21 12:21:15 -0400

    .16

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     testEnvironment: 'node',
     roots: ['<rootDir>/tests'],
-    testMatch: ['**/test*.ts'],
+    testMatch: ['**/*test.ts'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest'
     }
