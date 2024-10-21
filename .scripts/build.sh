07096dc277c98465106b23a13d1ae64357a7fbea
jj <jj@ondemandenv.dev>
2024-10-21 11:54:05 -0400

    .15

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -1,3 +1,11 @@
+
+set -ex
+
+echo "@ondemandenv:registry=https://npm.pkg.github.com/" >> .npmrc
+echo "@odmd-seed:registry=https://npm.pkg.github.com/" >> .npmrc
+echo "//npm.pkg.github.com/:_authToken=$github_token" >> .npmrc
+
+ls -ltarh
+cat .npmrc
+
 npm install
-npm run test
-npm run cdk-ls
