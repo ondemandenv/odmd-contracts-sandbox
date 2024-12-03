cbc965f1a6f63d87cc9c2971320a71362783341a
jj <jj@ondemandenv.dev>
2024-12-03 09:54:44 -0500

    .40/.43 +

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -1,4 +1,3 @@
-
 set -ex
 
 echo "@ondemandenv:registry=https://npm.pkg.github.com/" >> .npmrc
@@ -9,3 +8,17 @@ ls -ltarh
 cat .npmrc
 
 npm install
+
+PKG_NAME=$(jq -r '.name' package.json) && test "$PKG_NAME" != $ODMD_contractsLibPkgName || echo $PKG_NAME is good
+
+tsc --build --clean && tsc --build && npm publish
+git config user.name $GITHUB_RUN_ID
+git config user.email "odmd_wfl@ondemandenv.dev"
+PKG_VER=$(jq -r '.version' package.json)
+
+git tag "v$PKG_VER" && git tag "latest" -m "odmd" && git push origin --tags --force
+
+#${producer Id}.txt this is contract lib enver's single producer ...
+echo "$GITHUB_SHA,$PKG_NAME,$PKG_VER" > "$RUNNER_TEMP/contractsLibLatest.txt"
+
+npm dist-tag add $PKG_NAME@$PKG_VER $GITHUB_SHA --registry=https://npm.pkg.github.com
