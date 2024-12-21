e3e74a0da2277c328be6c3dc9c88975081c052a4
jj <jj@ondemandenv.dev>
2024-12-21 17:32:48 -0500

    .47

--- lib/repos/user-pool/CognitoUserPoolCdkOdmdBuild.ts
+++ lib/repos/user-pool/CognitoUserPoolCdkOdmdBuild.ts
@@ -1,11 +1,12 @@
 import {
-    OdmdBuild,
+    OdmdBuild, OdmdCrossRefConsumer,
     OdmdCrossRefProducer,
     OdmdEnverCdk,
     SRC_Rev_REF
 } from "@ondemandenv/contracts-lib-base";
 import {PolicyStatement} from "aws-cdk-lib/aws-iam";
 import {OndemandContractsSandbox} from "../../OndemandContractsSandbox";
+import {LlmChatLambdaS3Enver} from "../llm-chat_lambda_s3/LlmChatLambdaS3OdmdBuild";
 
 export class CognitoUserPoolEnver extends OdmdEnverCdk {
 
@@ -15,13 +16,26 @@ export class CognitoUserPoolEnver extends OdmdEnverCdk {
     readonly resourceUserPoolClientId: OdmdCrossRefProducer<CognitoUserPoolEnver>
     readonly oauth2RedirectUri: OdmdCrossRefProducer<CognitoUserPoolEnver>
 
-    constructor(owner: OdmdBuild<OdmdEnverCdk>, targetAWSAccountID: string, targetAWSRegion: string, targetRevision: SRC_Rev_REF) {
+
+    readonly llmChatCallbackUrl: OdmdCrossRefConsumer<CognitoUserPoolEnver, LlmChatLambdaS3Enver>[]
+    readonly llmChatLogoutUrl: OdmdCrossRefConsumer<CognitoUserPoolEnver, LlmChatLambdaS3Enver>[]
+
+    constructor(owner: CognitoUserPoolCdkOdmdBuild, targetAWSAccountID: string, targetAWSRegion: string, targetRevision: SRC_Rev_REF) {
         super(owner, targetAWSAccountID, targetAWSRegion, targetRevision);
         this.userPoolId = new OdmdCrossRefProducer(this, 'userpool-id')
         this.userPoolArn = new OdmdCrossRefProducer(this, 'userpool-arn')
         this.oauth2RedirectUri = new OdmdCrossRefProducer(this, 'oauth2-redirect-uri')
         this.oauthUserPoolClientId = new OdmdCrossRefProducer(this, 'oauth-userpool-clientId')
         this.resourceUserPoolClientId = new OdmdCrossRefProducer(this, 'resource-userpool-clientId')
+
+        this.llmChatCallbackUrl = []
+        this.llmChatLogoutUrl = []
+
+        owner.contracts.llmChatLambdaS3Cdk.envers.forEach(llmChat => {
+            this.llmChatCallbackUrl.push(new OdmdCrossRefConsumer(this, llmChat.node.id + '-callback', llmChat.callbackUrl))
+            this.llmChatLogoutUrl.push(new OdmdCrossRefConsumer(this, llmChat.node.id + '-logout', llmChat.logoutUrl))
+        })
+
     }
 
 }
