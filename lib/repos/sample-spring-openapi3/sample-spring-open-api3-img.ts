52893fc6fd18fcf89b1b414aaf01ea393b6a8904
jj <jj@ondemandenv.dev>
2025-03-05 18:45:24 -0500

    0.0.63 / 0.0.83

--- lib/repos/sample-spring-openapi3/sample-spring-open-api3-img.ts
+++ lib/repos/sample-spring-openapi3/sample-spring-open-api3-img.ts
@@ -7,16 +7,30 @@ import {
 import {RepositoryProps} from "aws-cdk-lib/aws-ecr";
 import {OndemandContractsSandbox} from "../../OndemandContractsSandbox";
 import {IGrantable} from "aws-cdk-lib/aws-iam";
+import {OdmdCrossRefProducer} from "@ondemandenv/contracts-lib-base/lib/model/odmd-cross-refs";
+import {AnyOdmdEnVer} from "@ondemandenv/contracts-lib-base/lib/model/odmd-enver";
 
 export class SampleSpringOpenApi3ImgEnver extends OdmdEnverCtnImg {
-    builtImgNameToRepoGrants: { [imgName: string]: [grantee: IGrantable, ...actions: string[]][]; };
+    builtImgNameToRepoGrants: {
+        [imgName: string]: [grantee: IGrantable | OdmdCrossRefProducer<AnyOdmdEnVer>, ...actions: string[]][];
+    };
 
-    constructor(owner: OdmdBuild<OdmdEnverCtnImg>, targetAWSAccountID: string, targetAWSRegion: string, targetRevision: SRC_Rev_REF) {
+    constructor(owner: SampleSpringOpenApi3Img, targetAWSAccountID: string, targetAWSRegion: string, targetRevision: SRC_Rev_REF) {
         super(owner, targetAWSAccountID, targetAWSRegion, targetRevision);
         this.builtImgNameToRepo = {[this.imgName]: {repositoryName: this.genRepoName('open3')}}
         this.ctnImgRefProducer = new CtnImgRefProducer(this, 'imgProducer', {repoPathPart: 'imgRepo'});
         this.builtImgNameToRepoProducer = {
             [this.imgName]: this.ctnImgRefProducer
+        }
+        this.builtImgNameToRepoGrants = {
+            [this.imgName]: [
+                [owner.contracts.eksCluster!.envers[0].defaultNodeGroupRoleArn,
+                    "ecr:BatchGetImage",
+                    "ecr:BatchCheckLayerAvailability",
+                    "ecr:GetDownloadUrlForLayer",
+                    "ecr:GetRepositoryPolicy"
+                ]
+            ]
         }
     }
 
