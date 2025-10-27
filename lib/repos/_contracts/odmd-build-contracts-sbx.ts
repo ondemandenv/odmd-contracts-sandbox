c1b4014600fd870ad09c28b66b08c5c30006f94d
Gary Yang <gary.y.7811@gmail.com>
2025-10-26 22:59:04 -0400

    .1.3

--- lib/repos/_contracts/odmd-build-contracts-sbx.ts
+++ lib/repos/_contracts/odmd-build-contracts-sbx.ts
@@ -10,6 +10,7 @@ import * as path from "path";
 class OdmdEnverContractsLibSeed extends OdmdEnverContractsLib {
     readonly enverContextMD = path.resolve(__dirname, 'docs', 'placeholder.md')
 }
+
 export class OdmdBuildContractsSbx extends OdmdBuildContractsLib<AccountsSbx, GithubReposSbx> {
     readonly serviceContextMD = path.resolve(__dirname, 'docs', 'placeholder.md')
     readonly serviceOverviewMD = path.resolve(__dirname, 'docs', 'placeholder.md')
@@ -44,13 +45,20 @@ export class OdmdBuildContractsSbx extends OdmdBuildContractsLib<AccountsSbx, Gi
                 this,
                 this.contracts.accounts.workspace0,
                 'us-west-2',
-                new SRC_Rev_REF("b", "__placeholder_us-west-2")
+                /*
+                /odmd-share/odmd-contracts-npm/b..usWest2/contractsLibLatest
+                */
+                new SRC_Rev_REF("b", "usWest2")
             ),
             new OdmdEnverContractsLibSeed(
                 this,
                 this.contracts.accounts.workspace0,
                 'us-east-1',
-                new SRC_Rev_REF("b", "__placeholder_us-east-1")
+
+                /*
+                /odmd-share/odmd-contracts-npm/b..usEast1/contractsLibLatest
+                */
+                new SRC_Rev_REF("b", "usEast1")
             )
         ];
     }
