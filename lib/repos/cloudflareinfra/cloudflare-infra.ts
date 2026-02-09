4b14ee2043e9fdcfb9e509bc474d08c2a1422caf
Gary Yang <gary.y.7811@gmail.com>
2026-02-08 20:53:27 -0500

    3 domains: newidClick, odmdDev, pubanatomyOrg

--- lib/repos/cloudflareinfra/cloudflare-infra.ts
+++ lib/repos/cloudflareinfra/cloudflare-infra.ts
@@ -1,6 +1,21 @@
-import {OdmdBuild, OdmdEnver, SRC_Rev_REF} from "@ondemandenv.dev/contracts-lib-base";
+import {OdmdBuild, OdmdEnver, SRC_Rev_REF, IOdmdEnver} from "@ondemandenv.dev/contracts-lib-base";
 import {OndemandContractsSandbox} from "../../OndemandContractsSandbox";
 import * as path from "path";
+
+export interface CloudflarePagesConfig {
+    projectName: string;
+    buildCommand?: string;
+    buildOutputDir?: string;
+    customDomain?: string;
+}
+
+export interface CloudflareFeatureSet {
+    emailRouting?: boolean;
+    pages?: CloudflarePagesConfig;
+    r2?: { bucketName: string };
+    d1?: { databaseName: string };
+    kv?: { namespaceName: string };
+}
 
 export class OdmdBuildCloudflareInfra extends OdmdBuild<OdmdEnverCloudflareInfra> {
     readonly serviceContextMD = path.resolve(__dirname, 'docs', 'SERVICE_OVERVIEW.md');
@@ -11,14 +26,44 @@ export class OdmdBuildCloudflareInfra extends OdmdBuild<OdmdEnverCloudflareInfra
         return this._envers;
     }
 
-    private _theOne: OdmdEnverCloudflareInfra;
-    get theOne(): OdmdEnverCloudflareInfra {
-        return this._theOne;
+    private _newidClick: OdmdEnverCloudflareInfra;
+    get newidClick(): OdmdEnverCloudflareInfra {
+        return this._newidClick;
+    }
+
+    private _ondemandenvDev: OdmdEnverCloudflareInfra;
+    get ondemandenvDev(): OdmdEnverCloudflareInfra {
+        return this._ondemandenvDev;
+    }
+
+    private _pubanatomyOrg: OdmdEnverCloudflareInfra;
+    get pubanatomyOrg(): OdmdEnverCloudflareInfra {
+        return this._pubanatomyOrg;
     }
 
     protected initializeEnvers(): void {
-        this._theOne = new OdmdEnverCloudflareInfra(this, this.contracts.accounts.workspace1, 'us-west-2', new SRC_Rev_REF('b', 'main'));
-        this._envers = [this._theOne];
+        this._newidClick = new OdmdEnverCloudflareInfra(
+            this, this.contracts.accounts.workspace1, 'us-west-2',
+            new SRC_Rev_REF('b', 'main'),
+            'newid.click', 'CLOUDFLARE_ZONE_ID_NEWID',
+            {emailRouting: true}
+        );
+
+        this._ondemandenvDev = new OdmdEnverCloudflareInfra(
+            this, this.contracts.accounts.workspace1, 'us-west-2',
+            new SRC_Rev_REF('b', 'ondemandenv-dev'),
+            'ondemandenv.dev', 'CLOUDFLARE_ZONE_ID_ODMD_DEV',
+            {emailRouting: true}
+        );
+
+        this._pubanatomyOrg = new OdmdEnverCloudflareInfra(
+            this, this.contracts.accounts.workspace1, 'us-west-2',
+            new SRC_Rev_REF('b', 'pubanatomy'),
+            'pubanatomy.org', 'CLOUDFLARE_ZONE_ID_PUBANATOMY',
+            {emailRouting: true}
+        );
+
+        this._envers = [this._newidClick, this._ondemandenvDev, this._pubanatomyOrg];
     }
 
     ownerEmail?: string | undefined;
@@ -36,9 +81,39 @@ export class OdmdBuildCloudflareInfra extends OdmdBuild<OdmdEnverCloudflareInfra
 export class OdmdEnverCloudflareInfra extends OdmdEnver<OdmdBuild<OdmdEnverCloudflareInfra>> {
     readonly enverContextMD = path.resolve(__dirname, 'docs', 'SERVICE_OVERVIEW.md');
 
-    readonly owner: OdmdBuildCloudflareInfra
+    readonly owner: OdmdBuildCloudflareInfra;
 
-    constructor(owner: OdmdBuildCloudflareInfra, targetAWSAccountID: string, targetAWSRegion: string, targetRevision: SRC_Rev_REF) {
+    readonly domainName: string;
+    readonly zoneIdSecretName: string;
+    readonly features: CloudflareFeatureSet;
+
+    constructor(
+        owner: OdmdBuildCloudflareInfra,
+        targetAWSAccountID: string,
+        targetAWSRegion: string,
+        targetRevision: SRC_Rev_REF,
+        domainName: string,
+        zoneIdSecretName: string,
+        features: CloudflareFeatureSet
+    ) {
         super(owner, targetAWSAccountID, targetAWSRegion, targetRevision);
+        this.domainName = domainName;
+        this.zoneIdSecretName = zoneIdSecretName;
+        this.features = features;
+    }
+
+    generateDynamicEnver(rev: SRC_Rev_REF, newInst: IOdmdEnver | undefined = undefined): IOdmdEnver {
+        if (!newInst) {
+            newInst = new OdmdEnverCloudflareInfra(
+                this.owner as OdmdBuildCloudflareInfra,
+                this.targetAWSAccountID,
+                this.targetAWSRegion,
+                rev,
+                this.domainName,
+                this.zoneIdSecretName,
+                this.features
+            ) as unknown as IOdmdEnver;
+        }
+        return super.generateDynamicEnver(rev, newInst);
     }
 }
